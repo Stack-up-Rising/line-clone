836565fdecc0addcb7a960618fb127cf8d6177c8
Kazuki Fukuyama <79068610+webzero07100805@users.noreply.github.com>
2022-03-20 22:20:25 +0900

    user変数の修正

--- src/App.js
+++ src/App.js
@@ -7,6 +7,6 @@ import Line from './components/Line';
 import SignIn from './components/SignIn';
 
 export default function App() {
-  const user = useAuthState(auth);
+  const [user] = useAuthState(auth);
   return <div>{user ? <Line /> : <SignIn />}</div>;
 }
