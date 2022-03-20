4af6a89b16f1034d7f9fb2585ef492b82076ae23
Kazuki Fukuyama <79068610+webzero07100805@users.noreply.github.com>
2022-03-21 00:25:19 +0900

    サインアウトの実装

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { auth } from '../firebase';
 import { Button } from '@material-ui/core';
+import CallIcon from '@mui/icons-material/Call';
 
 export default function SignOut() {
   return (
-    <div>
-      <Button onClick={() => auth.signOut()}>サインアウト</Button>
+    <div className='header'>
+      <Button
+        style={{ color: 'white', fontSize: '15px' }}
+        onClick={() => auth.signOut()}
+      >
+        サインアウト
+      </Button>
+      <h3>{auth.currentUser.displayName}</h3>
+      <CallIcon />
     </div>
   );
 }
-
