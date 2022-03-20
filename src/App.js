f2274e397cdacc399fb86188fc674c1539f2da6d
Kazuki Fukuyama <79068610+webzero07100805@users.noreply.github.com>
2022-03-20 22:07:57 +0900

    Lineコンポーネントの実装

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+// Reactでのユーザー認証
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase';
+
 import './App.css';
+import Line from './components/Line';
 import SignIn from './components/SignIn';
 
 export default function App() {
-  return (
-    <div>
-      <SignIn />
-    </div>
-  );
+  const user = useAuthState(auth);
+  return <div>{user ? <Line /> : <SignIn />}</div>;
 }
