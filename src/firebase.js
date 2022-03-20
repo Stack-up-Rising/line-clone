cdd2269a40f118c6ef08b6d69d27b0adc43330b1
Kazuki Fukuyama <79068610+webzero07100805@users.noreply.github.com>
2022-03-20 21:42:23 +0900

    Firebase初期化設定

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// Firebaseの初期化設定
+const firebaseApp = firebase.initializeApp({
+  apiKey: 'AIzaSyDq0V1YB-uBIAdCx0_wuvCWuEpCuZ_EYyM',
+  authDomain: 'line-clone-7a4c6.firebaseapp.com',
+  projectId: 'line-clone-7a4c6',
+  storageBucket: 'line-clone-7a4c6.appspot.com',
+  messagingSenderId: '941554062689',
+  appId: '1:941554062689:web:5c8f6eddbe057af9122120',
+  measurementId: 'G-7EQN1S0YKB',
+});
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export { db, auth };
